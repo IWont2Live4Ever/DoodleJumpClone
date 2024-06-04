407a44070ee21d8f9f15ef6afe2bb82c3d56c2cc
IWont2Live4Ever <147315438+IWont2Live4Ever@users.noreply.github.com>
2024-06-04 18:02:30 +0500

    Add files via upload

---
+++ DoodleJumpClone/DoodleJumpClone/PlayerCharacter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoodleJumpClone
+{
+    internal class PlayerCharacter
+    {
+        public Hitbox Hitbox { get; private set; }
+        public Direction Direction;
+        public Vector Velosity { get; set; }
+        public bool FlagJumpOpportunity;
+        public PlayerCharacter(int x, int y)
+        {
+            this.Hitbox = new Hitbox(
+                x,
+                y,
+                Settings.WidthOfPlayerCharacter_defoult,
+                Settings.HeightOfPlayerCharacter_defoult);
+            this.Direction = Direction.Left;
+            this.Velosity = Vector.GetZeroVector();
+            this.FlagJumpOpportunity = true;
+        }
+        public PlayerCharacter()
+        {
+            this.Hitbox = new Hitbox(
+                (Settings.WidthOfFild - Settings.WidthOfPlayerCharacter_defoult) / 2,
+                Settings.StartingHeight - Settings.HeightOfPlayerCharacter_defoult,
+                Settings.WidthOfPlayerCharacter_defoult,
+                Settings.HeightOfPlayerCharacter_defoult);
+            this.Direction = Direction.Left;
+            this.Velosity = new Vector();
+            this.FlagJumpOpportunity = true;
+        }
+        public void MoveRight()
+        {
+            this.Direction = Direction.Right;
+            this.Velosity.X += 5;
+            this.Correct();
+        }
+        public void MoveLeft()
+        {
+            this.Direction = Direction.Left;
+            this.Velosity.X -= 5;
+            this.Correct();
+        }
+        public void Jump()
+        {
+            if (this.FlagJumpOpportunity)
+            {
+                this.FlagJumpOpportunity = false;
+                this.Velosity.Y -= 10;
+                this.Correct();
+            }
+        }
+        public void Correct()
+        {
+            this.Velosity.GravityCorrection();
+
+            if (this.Hitbox.X >= Settings.WidthOfFild)
+            {
+                this.Hitbox.X = 0;
+            }
+            if (this.Hitbox.X <= 0)
+            {
+                this.Hitbox.X = Settings.WidthOfFild;
+            }
+        }
+    }
+}
---
+++ DoodleJumpClone/DoodleJumpClone/Vector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DoodleJumpClone
+{
+    public class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Vector()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double Length => Math.Sqrt(X * X + Y * Y);
+        public double Angle => Math.Atan2(Y, X);
+        public override string ToString() => $"X: {X}, Y: {Y}";
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X.GetHashCode() * 397) ^ Y.GetHashCode();
+            }
+        }
+
+        public static readonly Vector ZeroVector = new Vector(0, 0);
+        public static Vector operator -(Vector a, Vector b) => new Vector(a.X - b.X, a.Y - b.Y);
+
+        public static Vector operator *(Vector a, double k) => new Vector(a.X * k, a.Y * k);
+
+        public static Vector operator /(Vector a, double k) => new Vector   (a.X / k, a.Y / k);
+
+        public static Vector operator *(double k, Vector a) => a * k;
+
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.X + b.X, a.Y + b.Y);
+        
+        public bool Equals(Vector a, Vector b) => a.X == b.X && a.Y == b.Y;
+
+        public Vector Normalize() => Length > 0 ? this * (1 / Length) : this;
+
+        public Vector Rotate(double angle) =>
+            new Vector(
+                X * Math.Cos(angle) - Y * Math.Sin(angle),
+                X * Math.Sin(angle) + Y * Math.Cos(angle));
+
+        public Vector BoundTo(Vector size) => new Vector(Math.Max(0, Math.Min(size.X, X)), Math.Max(0, Math.Min(size.Y, Y)));
+
+        public static Vector GetZeroVector() => new Vector();
+
+        public void GravityCorrection() => new Vector(this.X * 0.9, this.Y + 10);
+    }
+}
